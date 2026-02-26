7970b1d78fb2f16a4f85750ba1f20c0b61fe3b0f
FLYBYME <mangoraft@gmail.com>
2026-02-25 21:15:10 -0500

    feat: Introduce a loading screen during IDE initialization and remove the HelloWorld extension.

--- public/extensions/1aece3ff-9df4-4226-b4a7-efcc885553a2/pending/bundle.js
+++
@@ -1,2 +0,0 @@
-function e(o){return console.log("Hello World Extension Activated!"),o.commands&&o.commands.registerCommand("hello.world",()=>{console.log("Hello from the command palette!")}),{name:"Hello World",status:"active"}}function l(){console.log("Hello World Extension Deactivated.")}export{e as activate,l as deactivate};
-//# sourceMappingURL=bundle.js.map
